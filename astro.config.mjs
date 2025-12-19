06bec45551042a18879c872c0ab4ab073964db10
Andrey Kuzovlev <andreykuzovlevv@gmail.com>
2025-12-19 20:08:58 +0800

    yo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import starlightAutoSidebar from 'starlight-auto-sidebar';
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: '{Name}',
+			plugins: [starlightAutoSidebar()],
 			social: [
 				{ icon: 'seti:dart', label: 'Dart', href: 'https://dart.dev/' },
 			],
